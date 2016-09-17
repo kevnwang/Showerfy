c79ea88f3fdcbdcc84e525e87de3f776c7841879
Kevin Wang <kwang21093@gmail.com>
2016-09-17 00:04:59 -0400

    Song select

---
+++ app/src/main/java/com/example/kevinwang/showerfy/SongSelectActivity.java
@@ -0,0 +1,16 @@
+package com.example.kevinwang.showerfy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by kevinwang on 9/17/16.
+ */
+public class SongSelectActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_songselect);
+
+    }
+}
