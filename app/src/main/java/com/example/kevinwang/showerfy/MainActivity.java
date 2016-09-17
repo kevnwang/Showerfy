4d22bdb671785f3f374c887b8e05142dacff91d5
Jo Chuang <josephch405@gmail.com>
2016-09-17 12:59:32 -0400

    fixed mplayer

--- app/src/main/java/com/example/kevinwang/showerfy/MainActivity.java
+++ app/src/main/java/com/example/kevinwang/showerfy/MainActivity.java
@@ -169,6 +169,7 @@ public class MainActivity extends Activity implements
                 mPlayer = Spotify.getPlayer(playerConfig, this, new Player.InitializationObserver() {
                     @Override
                     public void onInitialized(Player player) {
+                        mPlayer = player;
                         mPlayer.addConnectionStateCallback(MainActivity.this);
                         mPlayer.addPlayerNotificationCallback(MainActivity.this);
                         //mPlayer.play("spotify:track:2TpxZ7JUBn3uw46aR7qd6V");
